5ac9415ed7326123c122e27b330b7430858d654c
charles melo bispo de carvalho <charles.bispo@delbank.com.br>
2026-03-20 14:14:22 -0300

    add env example

---
+++ PHP/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sample/php-console',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '692d1c9b5cb182f0107afe1dcea53ecd7baa09a7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'delfinance/delfinance-api-sdk' => array(
+            'pretty_version' => 'v0.6.1',
+            'version' => '0.6.1.0',
+            'reference' => 'a497c069b03fedfe83bdfd39fd742fd7d43dc95c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../delfinance/delfinance-api-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sample/php-console' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '692d1c9b5cb182f0107afe1dcea53ecd7baa09a7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
